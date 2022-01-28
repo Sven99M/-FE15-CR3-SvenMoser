c1a63637d754c84d2b636abb90a6e472b5aa2a1e
Sven Moser <sven.mo99ser@gmail.com>
2022-01-28 17:30:22 +0100

    added script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-function calculateInvoice()
+function calculateInvoice() {
+    var prices = ["90€", "50€", "80€", "420€", "330€", "220€", "120€", "90€", "120€"];
+    console.log(prices);
+}
+console.log(prices);
